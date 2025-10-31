9147070c7f1e91af094b15bdca69c5eb668779d5
zhao lei <zhaolei@cwbio.com.cn>
2025-10-31 13:47:39 +0800

    - 修改bacteriaa_assembly的run.mk文件，调整nextflow运行参数； 设置sge qsub 参数；

--- pipeline_templates/bacteriaa_assembly/run.mk
+++ pipeline_templates/bacteriaa_assembly/run.mk
@@ -1,6 +1,14 @@
+#!/bin/bash
+#!/bin/bash
+#$ -V
+#$ -cwd
+#$ -q all.q
+#$ -l vf=20G
+#$ -pe smp 1
+
+
 source /nas02/software/conda/Miniconda3/miniconda3/bin/activate /nas02/project/huyifan/software/nextflow/v24.04.4
 nextflow -log nextflow.log \
-	run -qs 10 -resume /nas02/pipeline/bac_genome_assembly/latest_version/main.nf \
-	-profile sge \
+	run  /nas02/pipeline/bac_genome_assembly/latest_version/main.nf \
+	-profile sge -qs 20 -resume \
 	--csv ./input.tsv 
-	--outdir ./ 
